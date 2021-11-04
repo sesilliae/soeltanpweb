1103ea7da5ec78ef85d5056c846cd0be9a793348
Sesillia <90328063+sesilliae@users.noreply.github.com>
2021-11-04 23:42:10 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,245 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="index.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.css">
+    <link rel="stylesheet" href="text/css" href="fontawesome-free-5.15.4-web/css/all.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+    <title>Home-Sistem Informasi Kelautan</title>
+  </head>
+  <body>
+  <!--Navbar-->
+  <nav class="navbar navbar-expand-lg navbar-light shadow" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">
+          <img src="Gambar/download.png" alt="Logo" style="width:60px;">
+        </a>
+          <a class="navbar-brand" href="index.php">Sistem Informasi Kelautan</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto ">
+              <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="index.php" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Profil
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="Sejarah.Upi.Edu.php">Sejarah</a></li>
+                    <li><a class="dropdown-item" href="Visis&Misi.upi.edu.php">Visi & Misi</a></li>
+                    <li><a class="dropdown-item" href="Struktur Organiasai.upi.edu.php">Struktur Organisasi</a></li>
+                    <li><a class="dropdown-item" href="Dosen.upi.edu.php">Dosen</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#!" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Kemahasiswa
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="Himataska.upi.edu.php">HIMATASKA</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Tautan UPI
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="http://www.upi.edu/">Website Utama UPI</a></li>
+                    <li><a class="dropdown-item" href="http://ejournal.upi.edu/">Portal Jurnal</a></li>
+                    <li><a class="dropdown-item" href="http://perpustakaan.upi.edu/">Perpustakaan</a></li>
+                    <li><a class="dropdown-item" href="http://repository.upi.edu/">Repository</a></li>
+                    <li><a class="dropdown-item" href="http://digilib.upi.edu/">Digital Library</a></li>
+                    <li><a class="dropdown-item" href="http://pmb.upi.edu/">PMB</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Tautan Akademik
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <li><a class="dropdown-item" href="http://spot.upi.edu/">SPOT</a></li>
+                    <li><a class="dropdown-item" href="http://spada.upi.edu/">SPADA</a></li>
+                    <li><a class="dropdown-item" href="http://mrs.upi.edu/">MRS</a></li>
+                    <li><a class="dropdown-item" href="http://sino2.upi.edu/nilai/">SINO</a></li>
+                    <li><a class="dropdown-item" href="https://siak.upi.edu/sinndo/">SINNDO</a></li>
+                    <li><a class="dropdown-item" href="http://silabus.upi.edu/">SILABUS</a></li>
+                    <li><a class="dropdown-item" href="http://evaluasi-pbm.upi.edu/">Evaluasi PMB</a></li>
+                    <li><a class="dropdown-item" href="http://file.upi.edu/">Direktori File</a></li>
+                  </ul>
+                </li>
+          </ul>
+          </div>
+      </div>
+  </nav>
+
+  <!--Jumbotron-->
+  <section class="jumbotron" style="background-image: url(Gambar/pexels-jeremy-bishop-2765872.jpg);height: 500px; text-align: center;">
+    <br>
+    <h1 class="display-4 text-center">WELCOME TO MARINE INFORMATION SYSTEM DEPARTEMENT</h1>
+        <a class="btn btn-dark btn-lg" href="Sejarahindex.php
+        " role="button" >Jelajahi</a>
+  </section>
+  <!--Introduction-->
+  <div class="container">
+    <div class="grid">
+      <div class="row justify-content-center fs-6" id="intermezo">
+        <div class="col-5">
+          <iframe width="450" height="300" src="https://www.youtube.com/embed/uSpL9GR0QaI?start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div class="col-7 justify-content-center">
+            <h2>Tentang Kami</h2>
+                <p style="text-align: justify;"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe similique ex architecto quae ratione, rem hic nisi, optio enim expedita perspiciatis delectus rerum, vel error!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ipsa voluptates expedita asperiores dignissimos a voluptas, enim quidem possimus veritatis laboriosam natus inventore adipisci doloribus eius ipsum! Earum, assumenda saepe.
+                </p>
+                <a class="btn btn-primary" href="#" role="button">Selengkapnya</a>
+            </div>
+      </div>
+    </div>
+  </div>
+
+  <!--header-->
+  <header class="py-4" style="background-color: #cecece;" id="news">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder">Our Last News</h1>
+        </div>
+    </div>
+  </header>
+  <!--Last News-->
+  <div class="container" id="news">
+  <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col">
+      <div class="card">
+        <img src="Gambar/pexels-inchs-9325828.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Penyelam UPI</h5>
+          <p class="card-text">Penyelam bebas wanita terbaik di dunia dikhawatirkan tewas, setelah hilang ketika menyelam di lepas pantai Spanyol. Natalia Molchanova, penyelam bebas yang mengantongi 41 rekor dunia...</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="Gambar/pexels-nadia-chiesi-6023997.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Fakta menarik tentang ikan badudt</h5>
+          <p class="card-text">Siapa yang gak tahu film animasi Finding Nemo (2003)? Kemungkinan besar kamu pasti tahu dong film tersebut. Kalau ya, maka kamu pasti familier juga dengan gambar ikan jingga bergaris putih di atas.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="Gambar/pexels-irina-iriser-1086584.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Ubur-ubur spesies baru</h5>
+          <p class="card-text">Dua hari yag lalu, Mahasiswa Sistem Informasi Kelautan berhasil menemukan spesies Ubur-ubur yang baru. penemuan ini tentunya menggemparkan warga indonesia. khsususnya para ilmuan di dunia.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card">
+        <img src="Gambar/pexels-maahid-photos-7914461.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Kondisi Ombak</h5>
+          <p class="card-text">Universitas Pendidikan Indoensia kembali memecahkan rekor. mahasiswa Sistem Informasi Indonesia Meraih Mendali Emas dalam perlombaan ombak.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--Galeri-->
+<div class="container">
+  <hr size="30" style="margin-top: 80px;">
+  <div class="text-center text-dark">
+    <h1 class="display-4 fw-bolder">Our Galeri</h1>
+  </div> 
+  <div class="row mt-4">
+    <div class="item col-sm-6 col-md-4 mb-3">
+      <a href="galeri/pexels-inchs-7989770.jpg" class="fancybox" data-fancybox="galeri">
+        <img src="galeri/pexels-inchs-7989770.jpg" width="100%" height="100%">      
+      </a>
+    </div>
+    <div class="item col-sm-6 col-md-4 mb-3">
+      <a href="Galeri/pexels-kira-schwarz-9169378.jpg" class="fancybox" data-fancybox="galeri">
+        <img src="Galeri/pexels-kira-schwarz-9169378.jpg" width="100%" height="100%">      
+      </a>
+    </div>
+    <div class="item col-sm-6 col-md-4 mb-3">
+      <a href="Galeri/pexels-inchs-7941852.jpg" class="fancybox" data-fancybox="galeri">
+        <img src="Galeri/pexels-inchs-7941852.jpg" width="100%" height="100%">      
+      </a>
+    </div>
+    <div class="item col-sm-6 col-md-4 mb-3">
+      <a href="Galeri/pexels-pok-rie-4841219.jpg" class="fancybox" data-fancybox="galeri">
+        <img src="Galeri/pexels-pok-rie-4841219.jpg" width="100%" height="100%">      
+      </a>
+    </div>
+    <div class="item col-sm-6 col-md-4 mb-3">
+      <a href="Galeri/pexels-tom-fisk-1125979.jpg" class="fancybox" data-fancybox="galeri">
+        <img src="Galeri/pexels-tom-fisk-1125979.jpg" width="100%" height="100%">      
+      </a>
+    </div>
+    <div class="item col-sm-6 col-md-4 mb-3">
+      <a href="Galeri/pexels-tom-fisk-3057960.jpg" class="fancybox" data-fancybox="galeri">
+        <img src="Galeri/pexels-tom-fisk-3057960.jpg" width="100%" height="100%">      
+      </a>
+    </div>
+    <div class="item col-sm-6 col-md-4 mb-3">
+      <a href="Galeri/pexels-tom-fisk-3840441 (1).jpg" class="fancybox" data-fancybox="galeri">
+        <img src="Galeri/pexels-tom-fisk-3840441 (1).jpg" width="100%" height="100%">      
+      </a>
+    </div>
+    <div class="item col-sm-6 col-md-4 mb-3">
+      <a href="Galeri/pexels-yücel-özel-7878257.jpg" class="fancybox" data-fancybox="galeri">
+        <img src="Galeri/pexels-yücel-özel-7878257.jpg" width="100%" height="100%">      
+      </a>
+    </div>
+    <div class="item col-sm-6 col-md-4 mb-3">
+      <a href="Galeri/pexels-martin-damboldt-799091.jpg" class="fancybox" data-fancybox="galeri">
+        <img src="Galeri/pexels-martin-damboldt-799091.jpg" width="100%" height="100%">      
+      </a>
+    </div>
+   
+  </div>
+</div>
+<!--Footer-->
+<footer class="footer py-4">
+  <div class="container">
+      <div class="row align-items-center">
+        <hr>
+          <div class="col-lg-4 text-lg-start">Copyright © Universitas Pendidikan Indonesia 2021</div>
+          <div class="col-lg-4 my-3 my-lg-0">
+          </div>
+          <div class="col-lg-4 text-lg-end">
+              <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
+              <a class="link-dark text-decoration-none me-3" href="#!">Call-center</a>
+          </div>
+      </div>
+  </div>
+</footer>   
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="index.js"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+    <script type="text/javascript" src="index.js"></script>
+  </body>
+  </html>
